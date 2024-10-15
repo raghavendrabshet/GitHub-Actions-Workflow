8ddba53cdff9253d90caec85230dd94e302caf8e
raghavendrabshet <raghavendrabshet@gmail.com>
2024-10-15 14:44:28 +0530

    created RMQApi-tests and UnitTest file

---
+++ RMQApi/RMQApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace RMQApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
