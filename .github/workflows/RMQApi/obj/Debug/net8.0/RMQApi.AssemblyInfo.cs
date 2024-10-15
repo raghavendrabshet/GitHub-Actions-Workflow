c12660ffb756915baf7d0515bb2e08cd8085bce8
raghavendrabshet <raghavendrabshet@gmail.com>
2024-10-15 11:20:30 +0530

    updated rmq-build.yaml file

--- .github/workflows/RMQApi/obj/Debug/net8.0/RMQApi.AssemblyInfo.cs
+++ .github/workflows/RMQApi/obj/Debug/net8.0/RMQApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMQApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981d204014216b2cc4cb8df3d2a6a229e4e3b4ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96731244924aa27b1bd7aa4c737e6096baf0ebc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMQApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMQApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
