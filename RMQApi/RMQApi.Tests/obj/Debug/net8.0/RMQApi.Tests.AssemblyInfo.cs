c45070dc5521d5f5c353d50fad8c00b0d5a0c81d
raghavendrabshet <raghavendrabshet@gmail.com>
2024-10-15 16:11:47 +0530

    created coverage.cobertura.xml file

--- RMQApi/RMQApi.Tests/obj/Debug/net8.0/RMQApi.Tests.AssemblyInfo.cs
+++ RMQApi/RMQApi.Tests/obj/Debug/net8.0/RMQApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMQApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed03c428aaf1c83ea2d52e4df895989735b01a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947fe4b85b0d5770e88814d02ff34cb00986b1e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMQApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMQApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RMQApi/obj/Release/net8.0/RMQApi.AssemblyInfo.cs
+++ RMQApi/obj/Release/net8.0/RMQApi.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMQApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947fe4b85b0d5770e88814d02ff34cb00986b1e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa7470ad1a887cfd90ddff79cf255709c097430")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMQApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMQApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
